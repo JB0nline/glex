6e38c600d8e7873d8acde0bda9fd1d1fdc5b5dc8
JB0nline <jb0nline@hotmail.co.uk>
2016-01-11 12:14:03 +0000

    Added camera with rotation

--- src/CubeAsset.cc
+++ src/CubeAsset.cc
@@ -1,6 +1,6 @@
 #include "CubeAsset.h"
 
-CubeAsset::CubeAsset() {
+CubeAsset::CubeAsset() : model_matrix(glm::mat4(1.0)){
   // model coordinates, origin at centre.
   GLfloat vertex_buffer [] {
     -0.5, -0.5, -0.5
@@ -48,6 +48,8 @@ CubeAsset::CubeAsset() {
   glGenBuffers(1, &element_buffer_token);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_token);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * element_buffer_length, element_buffer, GL_STATIC_DRAW);
+
+  rotateX(60.0f);
 }
 
 CubeAsset::~CubeAsset() {
@@ -95,8 +97,13 @@ void CubeAsset::Draw(GLuint program_token) {
   GLuint position_attrib = glGetAttribLocation(program_token, "position");
   checkGLError();
 
+  GLuint model_uniform = glGetUniformLocation(program_token,"model");
+  checkGLError();
+
   glUseProgram(program_token);
   checkGLError();
+
+  glUniformMatrix4fv(model_uniform,1,false,glm::value_ptr(model_matrix));
 
   // use the previously transferred buffer as the vertex array.  This way
   // we transfer the buffer once -- at construction -- not on every frame.
@@ -124,4 +131,9 @@ void CubeAsset::Draw(GLuint program_token) {
   checkGLError();
 
   glDisableVertexAttribArray(position_attrib);
+}
+
+void CubeAsset::rotateX(float angle){
+	  glm::vec3 unit_x_axis(1.0,0.0,0.0);
+	  model_matrix = glm::rotate(this->model_matrix,angle,unit_x_axis);
 }
--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -1,10 +1,8 @@
 #include "GameWorld.h"
-#include "Pyramid.h"
 
 GameWorld::GameWorld (ApplicationMode mode) {
   asset_manager = std::make_shared<GameAssetManager>(mode);
- // asset_manager->AddAsset(std::make_shared<CubeAsset>());
-  asset_manager ->AddAsset(std::make_shared<Pyramid>());
+  asset_manager->AddAsset(std::make_shared<CubeAsset>());
 }
 
 
