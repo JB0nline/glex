afcbff63af3f2af989c7c8cd0fc1a637d85bdd84
JordanBarraclough <JB0nline@hotmail.co.uk>
2016-05-10 17:47:45 +0100

    Updated doxyfile settings as Doxygen settings were provided by colleague

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -1,6 +1,6 @@
 var NAVTREE =
 [
-  [ "CI224 - Assignment 2 - Arun Sharma", "index.html", [
+  [ "Glex Semester 2 Assignment", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
